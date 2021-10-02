e73cf3a2065416da9df4403ad3f06ac4131895fd
Matthew Lee <ml16847@bristol.ac.uk>
2021-10-02 11:01:08 +0100

    adiposity GWAS formatting

---
+++ scripts/000_adiposity_format/001_extract_instruments_adiposity.sh
@@ -0,0 +1,13 @@
+###########################
+#!/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=004:00:00
+
+export WORK_DIR=/newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data
+
+cd $WORK_DIR
+
+#loop through files in linux to extract GWAS significant SNPs
+ls *.txt | while read f; do awk -F"\t" 'NR==1{print;next}$9<5e-09' ${f} > /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/${f}_snps.txt; done;
+
+
