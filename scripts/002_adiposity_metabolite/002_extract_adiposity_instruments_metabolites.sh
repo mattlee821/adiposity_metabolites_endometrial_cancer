da8583aa4b8c176913eeb79ea4173f86912f444c
Matthew Lee <ml16847@bristol.ac.uk>
2021-10-02 19:36:00 +0100

    update

--- scripts/002_adiposity_metabolite/002_extract_adiposity_instruments_metabolites.sh
+++ scripts/002_adiposity_metabolite/002_extract_adiposity_instruments_metabolites.sh
@@ -14,4 +14,4 @@ ls *.txt | while read f; do grep -w -F -f /newhome/ml16847/001_projects/adiposit
 
 ls *.txt | while read f; do grep -w -F -f /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_GWAS/whr_snp_list.txt ${f} > /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whr/${f}_adiposity_instruments.txt; done;
 
-ls *.txt | while read f; do grep -w -F -f /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_GWAS/bf_snp_list.txt ${f} > /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whradjbmi/${f}_adiposity_instruments.txt; done;
+ls *.txt | while read f; do grep -w -F -f /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_GWAS/whradjbmi_snp_list.txt ${f} > /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whradjbmi/${f}_adiposity_instruments.txt; done;
