fd9f5be0476c22d6c26bd0543f5c7040b59f3870
Matthew Lee <ml16847@bristol.ac.uk>
2021-10-02 20:00:53 +0100

    upload

--- scripts/003_metabolite_endometrial/002_results_format_metabolite.R
+++ scripts/003_metabolite_endometrial/002_results_format_metabolite.R
@@ -6,9 +6,9 @@ setwd(directory_1)
 library(dplyr)
 
 # format metabolites
-metab <- read.table("007_metabolites_outcomes/data/metabolite_labels.txt", header = T, sep = "\t")
+metab <- read.table("adiposity_metabolites_endometrial_cancer/data/metabolite_labels.txt", header = T, sep = "\t")
 ## data ====
-data <- read.table("007_metabolites_outcomes/analysis/003_metabolite_endometrial/mr_results.txt", header = T, sep = "\t")
+data <- read.table("adiposity_metabolites_endometrial_cancer/analysis/003_metabolite_endometrial/mr_results.txt", header = T, sep = "\t")
 data$metabolite <- sub("_int_imputed.txt_snps.txt*", "", data$exposure)
 data$metabolite = gsub("_", "", data$metabolite)
 data$metabolite = gsub("%", "pct", data$metabolite)
@@ -16,5 +16,5 @@ data$metabolite = gsub("-", "", data$metabolite)
 data$metabolite = tolower(data$metabolite)
 data$UKB_label <- data$metabolite
 data <- left_join(data, metab, by = "UKB_label")
-write.table(data, "007_metabolites_outcomes/analysis/003_metabolite_endometrial/mr_results_formatted.txt", 
+write.table(data, "adiposity_metabolites_endometrial_cancer/analysis/003_metabolite_endometrial/mr_results_formatted.txt", 
             row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
