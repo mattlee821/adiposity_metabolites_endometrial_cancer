05f55dc39c4f9f869bbef88fdde518f0273153ba
Matthew Lee <ml16847@bristol.ac.uk>
2021-10-03 14:12:24 +0100

    extract metabolite ouctome data

--- scripts/002_adiposity_metabolite/003_extract_outcomes_whr.sh
+++ scripts/002_adiposity_metabolite/003_extract_outcomes_whr.sh
@@ -5,7 +5,7 @@
 #PBS -e log/
 #PBS -o log/
 
-export WORK_DIR=/newhome/ml16847/001_projects/007_metabolites_outcomes/data/adiposity_instruments_metabolites/whr/
+export WORK_DIR=/newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whr/
 
 cd $WORK_DIR
 
@@ -17,14 +17,13 @@ for file in *.txt; do
 done
 
 # move phenotype column to start column
-ls *.txt | while read f; do awk '{$1=$NF OFS $1;$NF=""}1' ${f} > /newhome/ml16847/001_projects/007_metabolites_outcomes/data/adiposity_instruments_metabolites/whr/${f}_final.txt; done;
-rm *.txt_adiposity_instruments.txt 
+ls *.txt | while read f; do awk '{$1=$NF OFS $1;$NF=""}1' ${f} > /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whr/extracted_data/${f}_final.txt; done;
 
 # combine data and add header
-head -1 /newhome/ml16847/001_projects/UKB_NMR_GWAS/instruments_female/Acetate_int_imputed.txt_snps.txt_instruments.txt > /newhome/ml16847/001_projects/007_metabolites_outcomes/data/adiposity_instruments_metabolites/header # get header
-for i in /newhome/ml16847/001_projects/007_metabolites_outcomes/data/adiposity_instruments_metabolites/whr/*.txt; do cat $i >> /newhome/ml16847/001_projects/007_metabolites_outcomes/data/adiposity_instruments_metabolites/whr_instruments_metabolites.txt; done
-cat /newhome/ml16847/001_projects/007_metabolites_outcomes/data/adiposity_instruments_metabolites/whr_instruments_metabolites.txt >> /newhome/ml16847/001_projects/007_metabolites_outcomes/data/adiposity_instruments_metabolites/header # append data onto header file
+head -1 /newhome/ml16847/001_projects/UKB_NMR_GWAS/instruments_female/Acetate_int_imputed.txt_snps.txt_instruments.txt > /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/header # get header
+for i in /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whr/extracted_data/*.txt; do cat $i >> /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whr_instruments_metabolites.txt; done
+cat /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whr_instruments_metabolites.txt >> /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/header # append data onto header file
 
 # get columns of interest
-cat /newhome/ml16847/001_projects/007_metabolites_outcomes/data/adiposity_instruments_metabolites/header | awk '{ print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15 }' > /newhome/ml16847/001_projects/007_metabolites_outcomes/data/adiposity_instruments_metabolites/metabolite_outcome_whr.txt
-rm /newhome/ml16847/001_projects/007_metabolites_outcomes/data/adiposity_instruments_metabolites/header /newhome/ml16847/001_projects/007_metabolites_outcomes/data/adiposity_instruments_metabolites/whr_instruments_metabolites.txt
+cat /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/header | awk '{ print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15 }' > /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/metabolite_outcome_whr.txt
+rm /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/header /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whr_instruments_metabolites.txt
