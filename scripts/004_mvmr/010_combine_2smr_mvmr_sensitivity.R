4739c54bb7cbfef3c4399afe376c089a9df87566
Matthew Lee <ml16847@bristol.ac.uk>
2021-11-08 13:59:50 +0000

    fstats

--- scripts/004_mvmr/010_combine_2smr_mvmr_sensitivity.R
+++ scripts/004_mvmr/010_combine_2smr_mvmr_sensitivity.R
@@ -3,16 +3,16 @@ rm(list=ls())
 directory_1 <- Sys.getenv("directory_1")
 setwd(directory_1)
 
-mvmr_fstat <- mvmr_sensitivity[,c("exposure", "outcome", "group", "fstat.exposure1", "method")]
-mvmr_fstat$exposure <- paste(mvmr_fstat$group,mvmr_fstat$outcome)
-mvmr_fstat <- mvmr_fstat[,c("exposure", "fstat.exposure1", "method")]
-colnames(mvmr_fstat)[2] <- "mean_fstat"
+## associated metabolites -> endometrial ====
+associated_metabolites <- read.table("adiposity_metabolites_endometrial_cancer/analysis/004_mvmr/associated_metabolites.txt", header = T, sep = "\t")
+associated_metabolites <- associated_metabolites[,1]
 
-
+# MR adiposity sensitivity ====
 adiposity_sensitivity <- read.table("adiposity_metabolites_endometrial_cancer/analysis/001_adiposity_endometrial/exposure_data.txt", header = T, sep = "\t")
 adiposity_sensitivity <- unique(adiposity_sensitivity[,c("exposure", "mean_fstat")])
 adiposity_sensitivity$method <- "Two-sample MR"
 
+# MR metabolite sensitivity ====
 metabolite_sensitivity <- read.table("adiposity_metabolites_endometrial_cancer/analysis/003_metabolite_endometrial/exposure_data.txt", header = T, sep = "\t")
 metabolite_sensitivity$exposure <- gsub("_int_imputed.txt_snps.txt", "", metabolite_sensitivity$exposure)
 metabolite_sensitivity$exposure = gsub("_", "", metabolite_sensitivity$exposure)
@@ -27,27 +27,16 @@ metabolite_sensitivity <-
   summarise(mean_fstat = mean(fstat))
 metabolite_sensitivity$method <- "Two-sample MR"
 
+# combine ====
+mr_sensitivity <- bind_rows(adiposity_sensitivity,metabolite_sensitivity)
+mr_sensitivity$adjusted <- mr_sensitivity$exposure
+
+# mvmr sensitivity ====
+data <- read.table("adiposity_metabolites_endometrial_cancer/analysis/004_mvmr/results/combined/combined_sensitivity.txt", header = T, sep = "\t")
+mvmr_fstat <- data[,c("exposure", "outcome", "adjusted", "fstat.exposure1", "method")]
+colnames(mvmr_fstat)[4] <- "mean_fstat"
+
+# combine sensitivity
 sensitivity <- bind_rows(adiposity_sensitivity, metabolite_sensitivity, mvmr_fstat)
-
-
-
-
-
-
-
-
-
-
-metabolites <- list.files(path = "adiposity_metabolites_endometrial_cancer/analysis/004_mvmr/results/adiposity/bmi/", pattern = "*txt")
-metabolites <- gsub("_int_imputed.txt", "", metabolites)
-metabolites <- gsub("_", "", metabolites)
-metabolites <- gsub("%", "pct", metabolites)
-metabolites <- gsub("-", "", metabolites)
-metabolites <- tolower(metabolites)
-
-data$method <- factor(data$method, levels = c("Two Sample MR", "Multivariable MR"))
-data$adjusted <- factor(data$adjusted, levels = c("BMI", "WHR", "WHRadjBMI", metabolites))
-data$OR <- exp(data$b)
-data$lower_ci <- data$OR - (1.96 * data$se)
-data$upper_ci <- data$OR + (1.96 * data$se)
-
+write.table(sensitivity, "adiposity_metabolites_endometrial_cancer/analysis/004_mvmr/results/combined/2smr_mvmr_formatted_fstats.txt", 
+            row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
