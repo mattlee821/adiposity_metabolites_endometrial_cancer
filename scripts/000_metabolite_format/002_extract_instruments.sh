1e430ee7dabda8cfd608d0dbbfb4091cb51eab19
Matthew Lee <ml16847@fvfxc19lhv2f.lan>
2022-02-03 13:41:54 +0000

    update

--- scripts/000_metabolite_format/002_extract_instruments.sh
+++ scripts/000_metabolite_format/002_extract_instruments.sh
@@ -11,6 +11,6 @@ cd /user/work/ml16847/UKB_NMR_GWAS/nmr_dat_female/
 ls *.txt | while read f; do awk -F"\t" 'NR==1{print;next}$14<5e-08' ${f} > /user/work/ml16847/UKB_NMR_GWAS/instruments/female/${f}_snps.txt; done;
 
 cd /user/work/ml16847/UKB_NMR_GWAS/nmr_dat_all/
-ls ${ALL}*.txt | while read f; do awk -F"\t" 'NR==1{print;next}$14<5e-08' ${f} > /user/work/ml16847/UKB_NMR_GWAS/instruments/all/${f}_snps.txt; done;
+ls *.txt | while read f; do awk -F"\t" 'NR==1{print;next}$14<5e-08' ${f} > /user/work/ml16847/UKB_NMR_GWAS/instruments/all/${f}_snps.txt; done;
 
 
