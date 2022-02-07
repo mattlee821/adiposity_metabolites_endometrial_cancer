ce763b18d02c56b4207a59d2890cd6ce74d23a16
Matthew Lee <ml16847@fvfxc19lhv2f.lan>
2022-02-07 13:26:14 +0000

    update

--- scripts/002_adiposity_metabolite/002_extract_adiposity_instruments_metabolites.sh
+++ scripts/002_adiposity_metabolite/002_extract_adiposity_instruments_metabolites.sh
@@ -9,18 +9,18 @@
 cd /user/work/ml16847/UKB_NMR_GWAS/nmr_dat_female/
 
 # loop through metabolite summary stats
-ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/bmi_snp_list_female.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/bmi/${f}_adiposity_instruments_female.txt; done;
+ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/bmi_snp_list_female.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/female/bmi/${f}_adiposity_instruments.txt; done;
 
-ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/whr_snp_list_female.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whr/${f}_adiposity_instruments_female.txt; done;
+ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/whr_snp_list_female.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/female/whr/${f}_adiposity_instruments.txt; done;
 
-ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/whradjbmi_snp_list_female.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whradjbmi/${f}_adiposity_instruments_female.txt; done;
+ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/whradjbmi_snp_list_female.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/female/whradjbmi/${f}_adiposity_instruments.txt; done;
 
 
 cd /user/work/ml16847/UKB_NMR_GWAS/nmr_dat_combined/
 
 # loop through metabolite summary stats
-ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/bmi_snp_list_combined.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/bmi/${f}_adiposity_instruments_combined.txt; done;
+ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/bmi_snp_list_combined.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/combined/bmi/${f}_adiposity_instruments.txt; done;
 
-ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/whr_snp_list_combined.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whr/${f}_adiposity_instruments_combined.txt; done;
+ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/whr_snp_list_combined.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/combined/whr/${f}_adiposity_instruments.txt; done;
 
-ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/whradjbmi_snp_list_combined.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whradjbmi/${f}_adiposity_instruments_combined.txt; done;
+ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/whradjbmi_snp_list_combined.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/combined/whradjbmi/${f}_adiposity_instruments.txt; done;
