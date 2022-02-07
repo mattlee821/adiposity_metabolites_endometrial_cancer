760ee89005a90a57cb3931f34a7f019bf8da60f6
Matthew Lee <ml16847@fvfxc19lhv2f.lan>
2022-02-07 13:20:51 +0000

    update

--- scripts/002_adiposity_metabolite/002_extract_adiposity_instruments_metabolites.sh
+++ scripts/002_adiposity_metabolite/002_extract_adiposity_instruments_metabolites.sh
@@ -1,17 +1,26 @@
-###########################
 #!/bin/bash
-#PBS -l nodes=1:ppn=1
-#PBS -l walltime=100:00:00
-#PBS -e log/
-#PBS -o log/
 
-export WORK_DIR=/newhome/ml16847/001_projects/UKB_NMR_GWAS/nmr_dat_female
+#SBATCH --job-name=MR-analysis
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=0-10:0:00
+#SBATCH --mem=1000M
 
-cd $WORK_DIR
+cd /user/work/ml16847/UKB_NMR_GWAS/nmr_dat_female/
 
 # loop through metabolite summary stats
-ls *.txt | while read f; do grep -w -F -f /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_GWAS/bmi_snp_list.txt ${f} > /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/bmi/${f}_adiposity_instruments.txt; done;
+ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/bmi_snp_list_female.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/bmi/${f}_adiposity_instruments_female.txt; done;
 
-ls *.txt | while read f; do grep -w -F -f /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_GWAS/whr_snp_list.txt ${f} > /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whr/${f}_adiposity_instruments.txt; done;
+ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/whr_snp_list_female.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whr/${f}_adiposity_instruments_female.txt; done;
 
-ls *.txt | while read f; do grep -w -F -f /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_GWAS/whradjbmi_snp_list.txt ${f} > /newhome/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whradjbmi/${f}_adiposity_instruments.txt; done;
+ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/whradjbmi_snp_list_female.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whradjbmi/${f}_adiposity_instruments_female.txt; done;
+
+
+cd /user/work/ml16847/UKB_NMR_GWAS/nmr_dat_combined/
+
+# loop through metabolite summary stats
+ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/bmi_snp_list_combined.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/bmi/${f}_adiposity_instruments_combined.txt; done;
+
+ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/whr_snp_list_combined.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whr/${f}_adiposity_instruments_combined.txt; done;
+
+ls *.txt | while read f; do grep -w -F -f /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity/whradjbmi_snp_list_combined.txt ${f} > /user/work/ml16847/001_projects/adiposity_metabolites_endometrial_cancer/data/adiposity_metabolites_outcomes/whradjbmi/${f}_adiposity_instruments_combined.txt; done;
