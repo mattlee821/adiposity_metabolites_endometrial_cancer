b003b86c44d3fa6e9373cbab9d6ac194e31855ed
Matthew Lee <ml16847@fvfxc19lhv2f.lan>
2022-02-07 12:10:07 +0000

    update

--- scripts/000_adiposity_format/001_extract_instruments_adiposity.sh
+++ scripts/000_adiposity_format/001_extract_instruments_adiposity.sh
@@ -9,8 +9,7 @@
 # set environment arguments 
 source ../../environment/environment.sh
 
-# unzip files
-gzip -d ${directory_1}adiposity_metabolites_endometrial_cancer/data/adiposity_GWAS/*.txt.gz
+cd ${directory_1}adiposity_metabolites_endometrial_cancer/data/adiposity_GWAS/
 
 #loop through files to extract GWAS significant SNPs
-ls ${directory_1}adiposity_metabolites_endometrial_cancer/data/adiposity_GWAS/*.txt | while read f; do awk -F" " 'NR==1{print;next}$9<5e-09' ${f} > ${directory_1}adiposity_metabolites_endometrial_cancer/data/adiposity/${f}_snps.txt; done;
+ls *.txt | while read f; do awk -F" " 'NR==1{print;next}$9<5e-09' ${f} > ${directory_1}adiposity_metabolites_endometrial_cancer/data/adiposity/${f}_snps.txt; done;
