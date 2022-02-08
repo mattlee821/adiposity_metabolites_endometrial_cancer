9cc9334c6ce1d435eabb3a4b583f86f65079a073
Matthew Lee <ml16847@fvfxc19lhv2f.lan>
2022-02-08 19:36:02 +0000

    update

---
+++ scripts/000_metabolite_format/004_clump_instruments.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=MR-analysis
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=0-10:0:00
+#SBATCH --mem=10000M
+
+# set environment arguments 
+source ../../environment/environment.sh
+
+# working directory
+cd ${directory_1}adiposity_metabolites_endometrial_cancer/scripts
+
+module add lang/r/4.0.3-bioconductor-gcc
+
+VAR1=004_clump_instruments_female.R
+VAR1=004_clump_instruments_clumped.R
+
+Rscript ${directory_1}adiposity_metabolites_endometrial_cancer/scripts/000_metabolite_formate/${VAR1}
+Rscript ${directory_1}adiposity_metabolites_endometrial_cancer/scripts/000_metabolite_formate/${VAR1}
---
+++ scripts/000_metabolite_format/004_clump_instruments_combined.R
@@ -0,0 +1,33 @@
+rm(list=ls())
+# MR analysis of measures of adiposity and metabolites 
+
+# environment ====
+## library ====
+#remotes::install_github("MRCIEU/TwoSampleMR")
+#remotes::install_github("mrcieu/ieugwasr")
+library(TwoSampleMR)
+
+## set environment ====
+directory_1 <- Sys.getenv("directory_1")
+setwd(directory_1)
+
+## extract exposure instruments ====
+exposure_data <- read_exposure_data("adiposity_metabolites_endometrial_cancer/data/metabolites/exposure_data_all.txt",
+                                    clump = F,
+                                    sep = " ",
+                                    snp_col = "SNP",
+                                    beta_col = "BETA",
+                                    se_col = "SE",
+                                    eaf_col = "A1FREQ",
+                                    effect_allele_col = "ALLELE1",
+                                    other_allele_col = "ALLELE0",
+                                    pval_col = "P_BOLT_LMM_INF",
+                                    phenotype = "phenotype",
+                                    min_pval = 5e-8)
+
+exposure_data <- clump_data(exposure_data,
+                                  clump_kb = 10000,
+                                  clump_r2 = 0.001)
+
+write.table(exposure_data, "adiposity_metabolites_endometrial_cancer/data/metabolites/exposure_data_clumped_combined.txt", 
+            row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t")
