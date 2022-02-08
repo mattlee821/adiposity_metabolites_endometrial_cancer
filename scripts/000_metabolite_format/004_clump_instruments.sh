26dad9412dd5a96a242b656dbc4a702781a6ed16
Matthew Lee <ml16847@fvfxc19lhv2f.lan>
2022-02-08 19:57:48 +0000

    update

--- scripts/000_metabolite_format/004_clump_instruments.sh
+++ scripts/000_metabolite_format/004_clump_instruments.sh
@@ -15,7 +15,7 @@ cd ${directory_1}adiposity_metabolites_endometrial_cancer/scripts
 module add lang/r/4.0.3-bioconductor-gcc
 
 VAR1=004_clump_instruments_female.R
-VAR2=004_clump_instruments_female.R
+VAR2=004_clump_instruments_combined.R
 
-Rscript ${directory_1}adiposity_metabolites_endometrial_cancer/scripts/000_metabolite_formate/${VAR1}
-Rscript ${directory_1}adiposity_metabolites_endometrial_cancer/scripts/000_metabolite_formate/${VAR2}
+Rscript ${directory_1}adiposity_metabolites_endometrial_cancer/scripts/000_metabolite_format/${VAR1}
+Rscript ${directory_1}adiposity_metabolites_endometrial_cancer/scripts/000_metabolite_format/${VAR2}
